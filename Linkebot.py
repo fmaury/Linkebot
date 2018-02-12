42f4a1bb05d0fc442ded2c61171eb3cd3835ead5
Francois MAURY <fmaury@e2r5p3.42.fr>
2018-02-12 16:30:41 +0100

    clean

--- Linkebot.py
+++ Linkebot.py
@@ -1,32 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import sys
+import getpass
 
-i = 0
-if len(sys.argv) < 4 :
-    print('Usage : python Linkebot.py --login "login" --password "password"')
-    sys.exit(0)
-if (sys.argv[1] == "--login" and sys.argv[3]  == "--password") or  (sys.argv[3] == "--login" and sys.argv[1]  == "--password") :
-  driver = webdriver.Firefox()
+def log(driver, login, passwd):
   driver.get("https://www.linkedin.com/uas/login?session_redirect=&goback=&trk=hb_signin")
   elem = driver.find_element_by_id("session_key-login")
-  elem.send_keys(sys.argv[2])
+  elem.send_keys(login)
   elem = driver.find_element_by_id("session_password-login")
-  elem.send_keys(sys.argv[4])
+  elem.send_keys(passwd)
   elem.send_keys(Keys.RETURN)
-  time.sleep(3)
+  time.sleep(6)
   elem = driver.find_element_by_id("mynetwork-nav-item")
   elem.click()
-  time.sleep(3)
+  time.sleep(6)
+
+def add(driver):
+  i = 0
   while 1 :
       if i == 0 :
           but = driver.find_elements_by_class_name("mn-pymk-list__action-container")
       but[i].click()
       if i != 0 and i % 6 == 0 :
           driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-          time.sleep(2)
+          time.sleep(4)
           i = -1
       i += 1
-else :
-    print('Usage : python Linkebot.py --login "login" --password "password"')
+
+if __name__ == "__main__":
+    login = str(raw_input("Email: "))
+    passwd = str(getpass.getpass())
+    driver = webdriver.Firefox()
+    log(driver, login, passwd)
+    add(driver)
+    driver.close()
